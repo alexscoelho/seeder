3cc9efe484f7123d80886f5f0ee6e4f993564683
Alexson Coelho <coelho@Alexsons-MacBook-Pro-2.local>
2022-05-05 20:26:43 -0400

    added support for products

--- README.md
+++ README.md
@@ -6,8 +6,12 @@ $ npm install
 
 ## Running the app
 
-In the root of your project add a "ss-customers.json" file and run:
+In the root of your project add a "customers.json" and "products.json" files and run:
 
 ```bash
-$ node main.js
+# to seed products
+$ node main.js products
+
+# to seed customers
+$ node main.js products
 ```
--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-const customers = require("./ss-customers.json");
+const customers = require("./customers.json");
+const products = require("./products.json");
+const baseUrl = "http://localhost:3333";
+
+const customerList = customers.QueryResponse.Customer;
+const productList = products.QueryResponse.Item;
+
+const [seedArg] = process.argv.slice(2);
 
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
@@ -17,19 +24,21 @@ function seedCustomers(customers) {
 }
 
 const createCustomer = (customer) => {
-  const customerName = customer.name || customer.company;
-  fetch("https://stepsolutionapi.herokuapp.com/customers", {
+  const customerName = customer.DisplayName || customer.CompanyName;
+  fetch(`${baseUrl}/customers`, {
     method: "POST",
     body: JSON.stringify({
-      ...customer,
-      email: customer.email || "N/A",
-      city: customer.city || "N/A",
-      state: customer.state || "N/A",
-      address: customer.address || "N/A",
-      address2: "see address",
+      name: customer?.GivenName + " " + customer?.FamilyName || "N/A",
+      phone: customer?.PrimaryPhone?.FreeFormNumber || "N/A",
+      email: customer?.PrimaryEmailAddr?.Address || "N/A",
+      city: customer?.BillAddr?.City || "N/A",
+      state: customer?.BillAddr?.CountrySubDivisionCode || "N/A",
+      address: customer?.BillAddr?.Line1 || "N/A",
+      address2: customer?.BillAddr?.Country || "N/A",
       tierLevel: "default",
-      company: customer.company || customer.name,
-      zipCode: parseInt(customer.zipcode) || 0,
+      company: customer?.CompanyName || customerName,
+      zipCode: parseInt(customer?.BillAddr?.PostalCode) || 0,
+      quickbooksId: customer?.Id,
     }),
     headers: { "Content-Type": "application/json" },
   })
@@ -43,4 +52,42 @@ const createCustomer = (customer) => {
     });
 };
 
-seedCustomers(customers);
+function seedProducts(products) {
+  products.forEach(async (product) => {
+    await sleep(2000);
+    createProducts(product);
+  });
+}
+
+const createProducts = (product) => {
+  const productName = product.Name;
+  fetch(`${baseUrl}/products`, {
+    method: "POST",
+    body: JSON.stringify({
+      quickbooksId: product?.Id,
+      sku: product?.Name,
+      name: product?.Name,
+      price: product?.UnitPrice,
+      imageUrl:
+        "https://static1.squarespace.com/static/5feb8101b5b33b527b373ebc/t/624deb72c3b55f64018575de/1649273714852/stdpsolution+icon-05.png",
+      description: product?.Description,
+      tierLevel: "A",
+      type: product?.Type === "Service" ? "SERVICE" : "NON_INVENTORY",
+    }),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((resp) => resp.json(resp))
+    .then((data) => {
+      if (data.statusCode > 400)
+        console.log(
+          `Products "${productName}"" could not be created, either exists or have missing fields`
+        );
+      else console.log(data);
+    });
+};
+
+if (seedArg === "products") {
+  seedProducts(productList);
+} else {
+  seedCustomers(customerList);
+}
